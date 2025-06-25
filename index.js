053e251cfe297045a69eda0b755f17c9142a008d
Vishnu <vishnu.cp.13@gmail.com>
2025-06-26 00:23:47 +0530

    added helmet and chenged some code

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 require('dotenv').config();
 const express = require('express');
-const app= express();
 const db = require("./config/db");
 const PORT =process.env.PORT
 const authRouter = require('./routes/auth-routes');
 const bookRouter = require('./routes/book-routes'); 
+const helmet = require('helmet');
+const app= express();
 
 
-
+app.use(helmet())
 app.use(express.json());
 
 app.use('/api',authRouter)
 app.use('/api/books',bookRouter)
-db();
 
 
-app.listen(PORT ,()=>{
-    console.log("SERVER Started at PORT",PORT);
-    
-})
+const startServer=async()=>{
+    try {
+        await db();
+ app.listen(PORT, () => {
+      console.log(`SERVER Started at PORT ${PORT}`);
+    })
+    } catch (error) {
+          console.error("DB connection failed", error);
+    process.exit(1);
+        
+    }
+}
+startServer()
+
